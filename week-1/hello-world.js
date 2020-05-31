40693955c6ced9711a3020a6b36a495a511526af
Wendy Leon <gugi00@hotmail.com>
2020-05-31 07:18:47 -0400

    Adding assignment to repository

--- week-1/hello-world.js
+++ week-1/hello-world.js
@@ -1,2 +1,12 @@
+/*
+============================================
+; Title: Assignment 1.3
+; Author: Professor Krasso
+; Date: 25 June 2017
+; Modified By: Wendy Leon
+; Description: Hello World with git push
+;===========================================
+*/
+
 
 console.log("Hello World, my name is Wen-D"); 
---
+++ week-1/leon-assignment-1.4.js
@@ -0,0 +1,58 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Professor Krasso
+; Date: 31 May 2020
+; Modified By: Wendy Leon
+; Description: Primitive Types
+;===========================================
+*/ 
+
+
+/*
+Create a variable named firstName and assign it a value of "your actual first name"
+Create a variable named lastName and assign it a value of "your actual last name"
+Create a variable named age and assign it a value of "your actual age"
+Create a variable named street and assign it a value of "some random street address"
+Create a variable named city and assign it a value of "some random city name"
+Create a variable named zipCode and assign it a value of "some random zip code"
+Use the console.log function to display the contents of each variable
+Run and test the program and make sure your output matches what I have 
+When you are happy with the output stage, commit, and push your work to GitHub
+*/
+
+//variable to hold my
+var firstName = "Wendy"
+
+//variable to hold my
+var lastName = "Leon"
+
+//variable to hold my
+var age = "42";
+
+//variable to hold my
+var street = "Hampton Street";
+
+//variable to hold my
+var city = "St Petersburg";
+
+//variable to hold my
+var zipCode = "34114";
+
+
+//code output 
+console.log(firstName);
+
+console.log(lastName);
+
+console.log(age);
+
+console.log(street);
+
+console.log(city);
+
+console.log(zipCode);
+
+
+
+
