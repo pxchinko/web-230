1c829996a71d04448cea523f2ad29209ec162507
Wen-D <pixelmamma@gmail.com>
2020-05-31 07:15:02 -0400

    Adding Attribution Comments

--- week-1/hello-world.js
+++ week-1/hello-world.js
@@ -1,2 +1,11 @@
+/*
+============================================
+; Title: Assignment 1.1
+; Author: Professor Krasso
+; Date: 31 May 2020
+; Modified By: Wendy Leon
+; Description: Hello World
+;===========================================
+*/ 
 
 console.log("Hello World, my name is Wen-D"); 
