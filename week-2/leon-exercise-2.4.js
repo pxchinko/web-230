4094434deb3147e54d325bff8af1ff1e617755e8
Wendy Leon <gugi00@hotmail.com>
2020-06-05 08:10:08 -0400

    Adding exercise 2.4

--- week-2/leon-exercise-2.4.js
+++ week-2/leon-exercise-2.4.js
@@ -29,26 +29,107 @@ console.log(header.display("Wendy", "Leon", "Exercise 2.4 - Functions"));
 
 console.log('\n');
 
-//function properties go here
+//Create a function called fullName with two parameters
+//(first and last name) and a return the two parameters as one string.
 
-myName.wendy = "Wendy";
-myName.leon = "Leon"
 
 //function
 
-function myName(){
-  return myName;
+function fullName(first, lastName){
+
+  return (first + " " + lastName);
 }
 
  //output
 
- console.log("Hello " + myName().wendy + " " + myName().leon);
+ console.log(fullName("Wendy","Leon"));
+
+ // test with variable
+
+ testFullName = fullName("Alfie","Leon");
+ console.log(testFullName);
+
+ // Create a function called dateWriter with three parameters
+ //(year, month, and day) and return a new date using the
+ //passed-in values.
+
+//function
+
+ function dateWriter (year, month, day){
+
+  return (new Date(day, month, year));
+}
+
+ //output
+
+ console.log(dateWriter(1,0,1978));
+
+// test with variable
+
+  testDateWriter = dateWriter(7,18,2010);
+  console.log(testDateWriter);
+
+// Create a function called formatNumber with two parameters
+//(number and numOfFixedPositions) and return the number
+//with the number of fixed positions you specified in the
+//second parameter.
+
+//function
+
+function formatNumber (number, numOfFixedPositions){
+
+  return (number.toFixed(numOfFixedPositions));
+}
+
+ //output
+
+ console.log(formatNumber(5,4));
+
+// test with variable
+
+testFormatNumber = formatNumber(4,2);
+console.log(testFormatNumber);
+
+//Create a function called convertToInt with one string
+//parameter.  Parse the string parameter and return the
+//value as an integer.
 
 
+//function
+
+function convertToInt (stringParameter){
+
+  return (parseInt(stringParameter));
+}
+
+ //output
+
+ console.log(convertToInt("5 is the number to be converted"));
+
+// test with variable
+
+testConvertToInt = parseInt("2nd test");
+console.log(testConvertToInt);
 
 
+//Create a function called convertToFloat with one string
+//parameter.  Parse the string parameter and return the
+//value as a float.
 
+//function
 
+function convertToFloat (stringParameter){
 
+  return (parseInt(stringParameter));
+}
+
+ //output
+
+ console.log(parseFloat("5.68 is the number to be converted"));
+
+// test with variable
+
+testConvertToFloat = parseFloat("4.63 miles");
+console.log(testConvertToFloat);
 
 
