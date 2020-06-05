0a0c869839432eeae2bae1ce45809268ac8a9f90
Wendy Leon <gugi00@hotmail.com>
2020-06-05 06:56:10 -0400

    Adding Exercise 2.4

---
+++ week-2/leon-exercise-2.4.js
@@ -0,0 +1,54 @@
+/*
+============================================
+; Title:  header.js
+; Author: Professor Krasso
+; Date:   5 June 2020
+; Modified by: Wendy Leon
+; Description: Functions with and without parameters
+;===========================================
+*/
+
+/*
+
+  FirstName LastName
+  Assignment 2.4
+  Today's Date
+
+  Hello my name is <your actual name goes here>!
+
+  Today's date is <today's date> and the current temperature is <33.6 - use the formatNumber function> degrees.
+
+  I am <your actual age - parsed int value> years old and my savings account goal is <your actual goal - parsed float value> dollars.
+
+*/
+
+
+var header = require('../header.js');
+
+console.log(header.display("Wendy", "Leon", "Exercise 2.4 - Functions"));
+
+console.log('\n');
+
+//function properties go here
+
+myName.wendy = "Wendy";
+myName.leon = "Leon"
+
+//function
+
+function myName(){
+  return myName;
+}
+
+ //output
+
+ console.log("Hello " + myName().wendy + " " + myName().leon);
+
+
+
+
+
+
+
+
+
