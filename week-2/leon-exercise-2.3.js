e30f3993e671f56ddb9391b1f1bdc1b44bbc9bf1
Wendy Leon <gugi00@hotmail.com>
2020-06-04 23:40:16 -0400

    Adding exercise 2.3 to repository

---
+++ week-2/leon-exercise-2.3.js
@@ -0,0 +1,51 @@
+/*
+============================================
+; Title:  header.js
+; Author: Professor Krasso
+; Date:   3 June 2020
+; Modified by: Wendy Leon
+; Description: Displays a formatted header
+;===========================================
+*/
+
+/*
+Expected Output:
+FirstName LastName
+Exercise 2.3
+Today's Date
+
+Hello FirstNames LastName!
+
+Hint: Use your personal header display function and
+refer to page 178 for implementing fuction properties
+*/
+
+var header = require('../header.js');
+
+console.log(header.display("Wendy", "Leon", "Exercise 2.3 - Header"));
+
+console.log('\n');
+
+//function properties go here
+
+myName.wendy = "Wendy";
+myName.leon = "Leon"
+
+//function
+
+function myName(){
+  return myName;
+}
+
+ //output
+
+ console.log("Hello " + myName().wendy + " " + myName().leon);
+
+
+
+
+
+
+
+
+
