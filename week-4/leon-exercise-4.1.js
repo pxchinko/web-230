06daffeedfa46e99c68cf99559bdc5375c1692aa
Wendy Leon <pixelmamma@gmail.com>
2020-06-16 10:51:24 -0400

    Adding Exercise 4.1

---
+++ week-4/leon-exercise-4.1.js
@@ -0,0 +1,24 @@
+/*
+============================================
+; Title:  4.1 Arrays - Two Errors
+; Author: Professor Krasso
+; Date:   15 June 2020
+; Modified by: Wendy Leon
+; Description: Arrays with Two Errors
+;===========================================
+*/
+
+/*
+Expected Output:
+
+Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+
+*/
+
+//Declare array
+
+var cars = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+
+//  //output
+
+ console.log ("Days of the Week: " + cars[]);
