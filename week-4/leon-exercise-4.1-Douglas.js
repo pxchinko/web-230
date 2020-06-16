e2ffd72e3cc9cc0a875941dd08a93a429db38e00
Wendy Leon <pixelmamma@gmail.com>
2020-06-16 11:22:06 -0400

    Adding 4.1 post #1

---
+++ week-4/leon-exercise-4.1-Douglas.js
@@ -0,0 +1,30 @@
+
+/* ============================================
+; Title: Assignment 4.1
+; Author: Professor Krasso
+; Date: 15 June 2020
+; Modified By: Douglas Jenkins
+; Description: 2 errors in the array
+;=========================================== */
+
+// List of Arrays that needs to be displayed
+
+// Two errors below
+
+var food = [ 'hot dog' , ' burger ', ' pizza ', ' tacos ', ' french fries '] ;
+
+//Function and Iteration creation to display the array
+
+function getFood ( arrFood ){
+
+var y =  arrFood.length;
+
+for ( var x = 0; x <  y ; x ++)
+
+console . log ( arrFood [ x ]);
+
+}
+
+// Calls the function and variable
+
+getFood ( food );
