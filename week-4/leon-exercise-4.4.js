88c2ffe9be52dc87d3c80e67d36e0bfa1fec7650
Wendy Leon <pixelmamma@gmail.com>
2020-06-19 21:54:21 -0400

    Adding edits to 4.4 file

--- week-4/leon-exercise-4.4.js
+++ week-4/leon-exercise-4.4.js
@@ -40,10 +40,15 @@ console.log(header.display("Wendy", "Leon", "Exercise 4.2 - Array Exercise - Fru
 
 console.log('\n');
 
-
+const ORIGINAL = " ORIGINAL ";
+const SORTED = " SORTED ";
+const SELECTED = " SELECTED ";
+const NEW_LINE = "\n"
+const ARRAY_WORD = "ARRAY ";
+const TWO_LINES = "--";
 // Create a string array of 5 states.
 
-var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California'];
+var states = ['Alabama', 'Nebraska', 'Iowa', 'California', 'Nevada'];
 
 // Create a function called getState with two string parameters
 
@@ -62,30 +67,52 @@ to test if the two string parameters match.  If they match, return true*/
 and pass-in the current element of the filter loop and the value to find
 (see page 154 of the courses textbook). */
 
-const x = "Alabama";
+/* This is what I came up doing an online research - much easier
+console.log (states.filter(findState => findState === X));*/
 
-// This is what I came up doing an online research
-
-console.log (states.filter(findState => findState === x));
-
-// following the instructions
-
-
+// following the instructions - this took a very long research
+//https://www.javascripttutorial.net/javascript-array-filter/
+/*
 console.log (
     states.filter (
       function (x) {
-        return getState (x, 'Alabama');
+        return getState (x, 'Iowa');
 
       }
     )
+);*/
+
+// Display the contents of the original array
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+console.log(TWO_LINES + ORIGINAL + ARRAY_WORD + TWO_LINES);
+states.forEach(element => {
+  console.log(element)
+});
+console.log(NEW_LINE);
+
+/*console.log(for ( var i = 0 ; i <= states.length - 1; i++ )
+{
+  console.log( states [i] );
+});*/
+
+// Sort and display the contents of the updated array
+
+//forEach( element => {console.log(sortedArray = states.sort());
+sortedArray = states.sort();
+console.log(TWO_LINES + SORTED + ARRAY_WORD + TWO_LINES);
+sortedArray.forEach (element => {console.log(element)});
+console.log(NEW_LINE);
+
+//Display the selected value
+console.log(TWO_LINES + "SELECTED VALUE" + TWO_LINES)
+console.log (
+    selected = states.filter (
+      function (x) {
+        return getState (x, 'Iowa');
+
+      }
+    ).toString()
 );
-
-
-
-
-
-
-
 
 
 
